b58eafb9a8cdc0b1a6989b24698a4e710f424b4a
Skornage <millerna@rose-hulman.edu>
2014-05-08 18:11:00 -0400

    Revert "blah"
    This reverts commit c39f65e91f3124d0747a27639dbd2df07ea70910.

--- Warring Kingdom/LoginForm.Designer.cs
+++ Warring Kingdom/LoginForm.Designer.cs
@@ -45,10 +45,9 @@
             this.usrLabel.BackColor = System.Drawing.Color.Transparent;
             this.usrLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.usrLabel.ForeColor = System.Drawing.Color.Lime;
-            this.usrLabel.Location = new System.Drawing.Point(155, 169);
-            this.usrLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.usrLabel.Location = new System.Drawing.Point(116, 137);
             this.usrLabel.Name = "usrLabel";
-            this.usrLabel.Size = new System.Drawing.Size(106, 20);
+            this.usrLabel.Size = new System.Drawing.Size(91, 17);
             this.usrLabel.TabIndex = 0;
             this.usrLabel.Text = "Username: ";
             // 
@@ -58,31 +57,28 @@
             this.pwdLabel.BackColor = System.Drawing.Color.Transparent;
             this.pwdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.pwdLabel.ForeColor = System.Drawing.Color.Lime;
-            this.pwdLabel.Location = new System.Drawing.Point(160, 214);
-            this.pwdLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pwdLabel.Location = new System.Drawing.Point(120, 174);
             this.pwdLabel.Name = "pwdLabel";
-            this.pwdLabel.Size = new System.Drawing.Size(103, 20);
+            this.pwdLabel.Size = new System.Drawing.Size(87, 17);
             this.pwdLabel.TabIndex = 1;
             this.pwdLabel.Text = "Password: ";
             // 
             // usrBox
             // 
             this.usrBox.BackColor = System.Drawing.SystemColors.Control;
-            this.usrBox.Location = new System.Drawing.Point(284, 167);
-            this.usrBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.usrBox.Location = new System.Drawing.Point(213, 136);
             this.usrBox.MaxLength = 16;
             this.usrBox.Name = "usrBox";
-            this.usrBox.Size = new System.Drawing.Size(159, 22);
+            this.usrBox.Size = new System.Drawing.Size(120, 20);
             this.usrBox.TabIndex = 2;
             // 
             // pwdBox
             // 
             this.pwdBox.BackColor = System.Drawing.SystemColors.Control;
-            this.pwdBox.Location = new System.Drawing.Point(284, 213);
-            this.pwdBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pwdBox.Location = new System.Drawing.Point(213, 173);
             this.pwdBox.MaxLength = 16;
             this.pwdBox.Name = "pwdBox";
-            this.pwdBox.Size = new System.Drawing.Size(159, 22);
+            this.pwdBox.Size = new System.Drawing.Size(120, 20);
             this.pwdBox.TabIndex = 3;
             this.pwdBox.UseSystemPasswordChar = true;
             // 
@@ -92,17 +88,16 @@
             this.loginTitle.BackColor = System.Drawing.Color.Transparent;
             this.loginTitle.Font = new System.Drawing.Font("Snap ITC", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginTitle.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.loginTitle.Location = new System.Drawing.Point(60, 55);
-            this.loginTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.loginTitle.Location = new System.Drawing.Point(45, 45);
             this.loginTitle.Name = "loginTitle";
-            this.loginTitle.Size = new System.Drawing.Size(497, 61);
+            this.loginTitle.Size = new System.Drawing.Size(392, 48);
             this.loginTitle.TabIndex = 4;
             this.loginTitle.Text = "Warring Kingdom";
             // 
             // LoginBtn
             // 
             this.LoginBtn.BackColor = System.Drawing.Color.Transparent;
-            this.LoginBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LoginBtn.BackgroundImage")));
+            this.LoginBtn.BackgroundImage = global::Warring_Kingdom.Properties.Resources.button;
             this.LoginBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.LoginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.LoginBtn.FlatAppearance.BorderSize = 0;
@@ -111,10 +106,9 @@
             this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.LoginBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.LoginBtn.Location = new System.Drawing.Point(333, 271);
-            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LoginBtn.Location = new System.Drawing.Point(250, 220);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(107, 37);
+            this.LoginBtn.Size = new System.Drawing.Size(80, 30);
             this.LoginBtn.TabIndex = 5;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = false;
@@ -126,10 +120,9 @@
             this.regLink.BackColor = System.Drawing.Color.Transparent;
             this.regLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.regLink.LinkColor = System.Drawing.Color.Cyan;
-            this.regLink.Location = new System.Drawing.Point(213, 279);
-            this.regLink.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.regLink.Location = new System.Drawing.Point(160, 227);
             this.regLink.Name = "regLink";
-            this.regLink.Size = new System.Drawing.Size(80, 20);
+            this.regLink.Size = new System.Drawing.Size(69, 17);
             this.regLink.TabIndex = 4;
             this.regLink.TabStop = true;
             this.regLink.Text = "Register";
@@ -141,20 +134,19 @@
             this.authorLabel.BackColor = System.Drawing.Color.Transparent;
             this.authorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.authorLabel.ForeColor = System.Drawing.Color.White;
-            this.authorLabel.Location = new System.Drawing.Point(273, 417);
-            this.authorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.authorLabel.Location = new System.Drawing.Point(205, 339);
             this.authorLabel.Name = "authorLabel";
-            this.authorLabel.Size = new System.Drawing.Size(339, 17);
+            this.authorLabel.Size = new System.Drawing.Size(266, 13);
             this.authorLabel.TabIndex = 6;
             this.authorLabel.Text = "- by Hanwei Zhang, Jiaqi Liu, and Noah Miller";
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Warring_Kingdom.Properties.Resources.login;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(643, 437);
+            this.ClientSize = new System.Drawing.Size(484, 362);
             this.Controls.Add(this.authorLabel);
             this.Controls.Add(this.regLink);
             this.Controls.Add(this.LoginBtn);
@@ -167,12 +159,11 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(661, 482);
-            this.MinimumSize = new System.Drawing.Size(661, 482);
+            this.MaximumSize = new System.Drawing.Size(500, 400);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "LoginForm";
-            this.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " Login";
             this.ResumeLayout(false);
